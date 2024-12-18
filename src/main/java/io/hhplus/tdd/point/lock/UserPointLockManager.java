fe307337f384625a9161bd476c801c1e50903c61
nakyoung <nacoon53@gmail.com>
2024-12-19 04:07:24 +0900

    feat: 동시성 제어에 대한 기능 추가 및 테스트 코드 작성

---
+++ src/main/java/io/hhplus/tdd/point/lock/UserPointLockManager.java
@@ -0,0 +1,35 @@
+package io.hhplus.tdd.point.lock;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Component
+public class UserPointLockManager {
+    private static final Map<Long, Lock> userPointLocks = new ConcurrentHashMap<>();
+
+    private static Lock getLock(long userId) {
+        return userPointLocks.computeIfAbsent(userId, k -> new ReentrantLock());
+    }
+
+    public static void lock(long userId) {
+        Lock lock = getLock(userId);
+
+        lock.lock();
+    }
+
+    public static void unlock(long userId) {
+        Lock lock = getLock(userId);
+
+        if(ObjectUtils.isEmpty(lock)) {
+            throw new RuntimeException("존재하지 않는 Lock 객체 입니다.");
+        }
+
+        lock.unlock();
+        //userPointLocks.remove(userId); //TODO remove 안 하면 Map에 계속 데이터가 쌓일텐데 이 부분에 대한 처리는 어떻게 할 지에 대해 생각하기
+    }
+}
--- src/main/java/io/hhplus/tdd/point/service/UserPointService.java
+++ src/main/java/io/hhplus/tdd/point/service/UserPointService.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.lock.UserPointLockManager;
 import io.hhplus.tdd.point.repository.UserPointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,19 +29,25 @@ public class UserPointService {
         if(amount < 0 || amount > MAXIMUM_POINT) { //todo(나경) 이거는 객체의 책임으로 가져갈수 있을 것 같음. 더 고민해 보기
             throw new IllegalArgumentException("적립할 포인트 금액이 유효하지 않습니다.");
         }
+        UserPointLockManager.lock(id);
 
-        UserPoint savedUserPoint = getPointAmount(id);
+        try {
+            UserPoint savedUserPoint = getPointAmount(id);
 
-        long willUpdatePointAmount = savedUserPoint.point() + amount;
-        if (willUpdatePointAmount > MAXIMUM_POINT) {
-            throw new IllegalArgumentException("최대 보유 가능한 포인트 잔액을 초과합니다.");
+            long willUpdatePointAmount = savedUserPoint.point() + amount;
+            if (willUpdatePointAmount > MAXIMUM_POINT) {
+                throw new IllegalArgumentException("최대 보유 가능한 포인트 잔액을 초과합니다.");
+            }
+
+            UserPoint updatedUserPoint = userPointRepository.updateUserPoint(id, willUpdatePointAmount);
+
+            userPointRepository.insertPointHistoryofCharge(id, amount);
+
+            return updatedUserPoint;
+
+        } finally {
+            UserPointLockManager.unlock(id);
         }
-
-        UserPoint updatedUserPoint = userPointRepository.updateUserPoint(id, willUpdatePointAmount);
-
-        userPointRepository.insertPointHistoryofCharge(id, amount);
-
-        return updatedUserPoint;
     }
 
     public UserPoint useUserPoint(long id, long amount) throws IllegalArgumentException{
@@ -48,17 +55,24 @@ public class UserPointService {
             throw new IllegalArgumentException("사용할 포인트 금액이 유효하지 않습니다.");
         }
 
-        UserPoint savedUserPoint = getPointAmount(id);
+        UserPointLockManager.lock(id);
 
-        long willUpdatePointAmount = savedUserPoint.point() - amount;
-        if (willUpdatePointAmount < 0) {
-            throw new IllegalArgumentException("보유한 포인트 금액을 초과하여 사용할 수 없습니다.");
+        try {
+            UserPoint savedUserPoint = getPointAmount(id);
+
+            long willUpdatePointAmount = savedUserPoint.point() - amount;
+            if (willUpdatePointAmount < 0) {
+                throw new IllegalArgumentException("보유한 포인트 금액을 초과하여 사용할 수 없습니다.");
+            }
+
+            UserPoint updatedUserPoint = userPointRepository.updateUserPoint(id, willUpdatePointAmount);
+
+            userPointRepository.insertPointHistoryofUse(id, amount);
+
+            return updatedUserPoint;
+
+        } finally {
+            UserPointLockManager.unlock(id);
         }
-
-        UserPoint updatedUserPoint = userPointRepository.updateUserPoint(id, willUpdatePointAmount);
-
-        userPointRepository.insertPointHistoryofUse(id, amount);
-
-        return updatedUserPoint;
     }
 }
